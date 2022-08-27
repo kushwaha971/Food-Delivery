05ae66c51fc6190f920d6ede43583e2cbe120524
Akash <kushwahaakash971gmail.com>
2022-08-27 15:09:56 +0530

    Closed Offer UI

--- src/component/offers/OfferClosed.jsx
+++ src/component/offers/OfferClosed.jsx
@@ -82,7 +82,7 @@ function OfferClosed() {
               marginTop: "40px",
             }}
           >
-            Keep connected and follow us on Instagram:{" "}
+            Keep connected and follow us on Instagram:<br></br>{" "}
             <span>
               <a
                 href="https://www.instagram.com/pluto_india_food/tagged/"
